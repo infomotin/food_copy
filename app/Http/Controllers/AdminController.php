0c7fe89bf5794665d9a9bf34ab86375523703cbd
infomotin <motin.mmk.jr@gmail.com>
2024-09-28 13:51:31 +0600

    Back End  Template Update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
 
     public function AdminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
         // return response()->json(['data' => 'Admin Dashboard']);
     }
 
