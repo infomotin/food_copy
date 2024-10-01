6b50c92605d5d5cc66c4596da9a57e38c978d77d
infomotin <motin.mmk.jr@gmail.com>
2024-10-01 11:00:52 +0600

    today

---
+++ resources/views/frontend/layouts/ads.blade.php
@@ -0,0 +1,26 @@
+<section class="pt-5 pb-5 bg-white section homepage-add-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-6">
+                <div class="products-box">
+                    <a href="listing.html"><img alt="" src="img/pro1.jpg" class="rounded img-fluid"></a>
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="products-box">
+                    <a href="listing.html"><img alt="" src="img/pro2.jpg" class="rounded img-fluid"></a>
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="products-box">
+                    <a href="listing.html"><img alt="" src="img/pro3.jpg" class="rounded img-fluid"></a>
+                </div>
+            </div>
+            <div class="col-md-3 col-6">
+                <div class="products-box">
+                    <a href="listing.html"><img alt="" src="img/pro4.jpg" class="rounded img-fluid"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,296 @@
+<section class="pt-5 pb-5 section products-section">
+    <div class="container">
+        <div class="text-center section-header">
+            <h2>Popular Brands</h2>
+            <p>Top restaurants, cafes, pubs, and bars in Ludhiana, based on trends</p>
+            <span class="line"></span>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="owl-carousel owl-carousel-four owl-theme">
+                    <div class="item">
+                        <div class="overflow-hidden bg-white rounded shadow-sm list-card h-100 position-relative">
+                            <div class="list-card-image">
+                                <div class="star position-absolute"><span class="badge badge-success"><i
+                                            class="icofont-star"></i> 3.1 (300+)</span></div>
+                                <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i
+                                            class="icofont-heart"></i></a></div>
+                                <div class="member-plan position-absolute"><span
+                                        class="badge badge-dark">Promoted</span></div>
+                                <a href="detail.html">
+                                    <img src="img/list/1.png" class="img-fluid item-img">
+                                </a>
+                            </div>
+                            <div class="p-3 position-relative">
+                                <div class="list-card-body">
+                                    <h6 class="mb-1"><a href="detail.html" class="text-black">World Famous</a></h6>
+                                    <p class="mb-3 text-gray">North Indian • American • Pure veg</p>
+                                    <p class="mb-3 text-gray time"><span
+                                            class="pt-1 pb-1 pl-2 pr-2 rounded-sm bg-light text-dark"><i
+                                                class="icofont-wall-clock"></i> 20–25 min</span> <span
+                                            class="float-right text-black-50"> $250 FOR TWO</span></p>
+                                </div>
+                                <div class="list-card-badge">
+                                    <span class="badge badge-success">OFFER</span> <small>65% off | Use Coupon
+                                        OSAHAN50</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="overflow-hidden bg-white rounded shadow-sm list-card h-100 position-relative">
+                            <div class="list-card-image">
+                                <div class="star position-absolute"><span class="badge badge-warning"><i
+                                            class="icofont-star"></i> 3.1 (300+)</span></div>
+                                <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i
+                                            class="icofont-heart"></i></a></div>
+                                <div class="member-plan position-absolute"><span
+                                        class="badge badge-dark">Promoted</span></div>
+                                <a href="detail.html">
+                                    <img src="img/list/3.png" class="img-fluid item-img">
+                                </a>
+                            </div>
+                            <div class="p-3 position-relative">
+                                <div class="list-card-body">
+                                    <h6 class="mb-1"><a href="detail.html" class="text-black">Bite Me Sandwiches</a>
+                                    </h6>
+                                    <p class="mb-3 text-gray">North Indian • Indian • Pure veg</p>
+                                    <p class="mb-3 text-gray time"><span
+                                            class="pt-1 pb-1 pl-2 pr-2 rounded-sm bg-light text-dark"><i
+                                                class="icofont-wall-clock"></i> 15–25 min</span> <span
+                                            class="float-right text-black-50"> $100 FOR TWO</span></p>
+                                </div>
+                                <div class="list-card-badge">
+                                    <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon
+                                        OSAHAN50</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="overflow-hidden bg-white rounded shadow-sm list-card h-100 position-relative">
+                            <div class="list-card-image">
+                                <div class="star position-absolute"><span class="badge badge-success"><i
+                                            class="icofont-star"></i> 3.1 (300+)</span></div>
+                                <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i
+                                            class="icofont-heart"></i></a></div>
+                                <div class="member-plan position-absolute"><span
+                                        class="badge badge-danger">Promoted</span></div>
+                                <a href="detail.html">
+                                    <img src="img/list/6.png" class="img-fluid item-img">
+                                </a>
+                            </div>
+                            <div class="p-3 position-relative">
+                                <div class="list-card-body">
+                                    <h6 class="mb-1"><a href="detail.html" class="text-black">The osahan Restaurant
+                                        </a>
+                                    </h6>
+                                    <p class="mb-3 text-gray">North • Hamburgers • Pure veg</p>
+                                    <p class="mb-3 text-gray time"><span
+                                            class="pt-1 pb-1 pl-2 pr-2 rounded-sm bg-light text-dark"><i
+                                                class="icofont-wall-clock"></i> 15–25 min</span> <span
+                                            class="float-right text-black-50"> $500 FOR TWO</span></p>
+                                </div>
+                                <div class="list-card-badge">
+                                    <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon
+                                        OSAHAN50</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="overflow-hidden bg-white rounded shadow-sm list-card h-100 position-relative">
+                            <div class="list-card-image">
+                                <div class="star position-absolute"><span class="badge badge-success"><i
+                                            class="icofont-star"></i> 3.1 (300+)</span></div>
+                                <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i
+                                            class="icofont-heart"></i></a></div>
+                                <div class="member-plan position-absolute"><span
+                                        class="badge badge-dark">Promoted</span></div>
+                                <a href="detail.html">
+                                    <img src="img/list/8.png" class="img-fluid item-img">
+                                </a>
+                            </div>
+                            <div class="p-3 position-relative">
+                                <div class="list-card-body">
+                                    <h6 class="mb-1"><a href="detail.html" class="text-black">Polo Lounge
+                                        </a>
+                                    </h6>
+                                    <p class="mb-3 text-gray">North Indian • Indian • Pure veg</p>
+                                    <p class="mb-3 text-gray time"><span
+                                            class="pt-1 pb-1 pl-2 pr-2 rounded-sm bg-light text-dark"><i
+                                                class="icofont-wall-clock"></i> 15–25 min</span> <span
+                                            class="float-right text-black-50"> $250 FOR TWO</span></p>
+                                </div>
+                                <div class="list-card-badge">
+                                    <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon
+                                        OSAHAN50</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="overflow-hidden bg-white rounded shadow-sm list-card h-100 position-relative">
+                            <div class="list-card-image">
+                                <div class="star position-absolute"><span class="badge badge-success"><i
+                                            class="icofont-star"></i> 3.1 (300+)</span></div>
+                                <div class="favourite-heart text-danger position-absolute"><a href="detail.html"><i
+                                            class="icofont-heart"></i></a></div>
+                                <div class="member-plan position-absolute"><span
+                                        class="badge badge-dark">Promoted</span></div>
+                                <a href="detail.html">
+                                    <img src="img/list/9.png" class="img-fluid item-img">
+                                </a>
+                            </div>
+                            <div class="p-3 position-relative">
+                                <div class="list-card-body">
+                                    <h6 class="mb-1"><a href="detail.html" class="text-black">Jack Fry's
+                                        </a>
+                                    </h6>
+                                    <p class="mb-3 text-gray">North Indian • Indian • Pure veg</p>
+                                    <p class="mb-3 text-gray time"><span
+                                            class="pt-1 pb-1 pl-2 pr-2 rounded-sm bg-light text-dark"><i
+                                                class="icofont-wall-clock"></i> 15–25 min</span> <span
+                                            class="float-right text-black-50"> $250 FOR TWO</span></p>
+                                </div>
+                                <div class="list-card-badge">
+                                    <span class="badge badge-danger">OFFER</span> <small>65% off | Use Coupon
+                                        OSAHAN50</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@include('frontend.layouts.member')
+
+<section class="pt-5 pb-5 text-center bg-white section">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h5 class="m-0">Operate food store or restaurants? <a href="login.html">Work With Us</a></h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5 pb-5 footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-12 col-sm-12">
+                <h6 class="mb-3">Subscribe to our Newsletter</h6>
+                <form class="mb-1 newsletter-form">
+                    <div class="input-group">
+                        <input type="text" placeholder="Please enter your email" class="form-control">
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-primary">
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <p><a class="text-info" href="register.html">Register now</a> to get updates on <a
+                        href="offers.html">Offers and Coupons</a></p>
+                <div class="app">
+                    <p class="mb-2">DOWNLOAD APP</p>
+                    <a href="#">
+                        <img class="img-fluid" src="img/google.png">
+                    </a>
+                    <a href="#">
+                        <img class="img-fluid" src="img/apple.png">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-1 col-sm-6 mobile-none">
+            </div>
+            <div class="col-md-2 col-6 col-sm-4">
+                <h6 class="mb-3">About OE</h6>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Culture</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-6 col-sm-4">
+                <h6 class="mb-3">For Foodies</h6>
+                <ul>
+                    <li><a href="#">Community</a></li>
+                    <li><a href="#">Developers</a></li>
+                    <li><a href="#">Blogger Help</a></li>
+                    <li><a href="#">Verified Users</a></li>
+                    <li><a href="#">Code of Conduct</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 m-none col-4 col-sm-4">
+                <h6 class="mb-3">For Restaurants</h6>
+                <ul>
+                    <li><a href="#">Advertise</a></li>
+                    <li><a href="#">Add a Restaurant</a></li>
+                    <li><a href="#">Claim your Listing</a></li>
+                    <li><a href="#">For Businesses</a></li>
+                    <li><a href="#">Owner Guidelines</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5 pb-5 bg-white footer-bottom-search">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <p class="text-black">POPULAR COUNTRIES</p>
+                <div class="search-links">
+                    <a href="#">Australia</a> | <a href="#">Brasil</a> | <a href="#">Canada</a> | <a
+                        href="#">Chile</a> | <a href="#">Czech Republic</a> | <a href="#">India</a> | <a
+                        href="#">Indonesia</a> | <a href="#">Ireland</a> | <a href="#">New Zealand</a> | <a
+                        href="#">United Kingdom</a> | <a href="#">Turkey</a> | <a href="#">Philippines</a> | <a
+                        href="#">Sri Lanka</a> | <a href="#">Australia</a> | <a href="#">Brasil</a> | <a
+                        href="#">Canada</a> | <a href="#">Chile</a> | <a href="#">Czech Republic</a> | <a
+                        href="#">India</a> | <a href="#">Indonesia</a> | <a href="#">Ireland</a> | <a href="#">New
+                        Zealand</a> | <a href="#">United Kingdom</a> | <a href="#">Turkey</a> | <a
+                        href="#">Philippines</a> | <a href="#">Sri Lanka</a><a href="#">Australia</a> | <a
+                        href="#">Brasil</a> | <a href="#">Canada</a> | <a href="#">Chile</a> | <a href="#">Czech
+                        Republic</a> | <a href="#">India</a> | <a href="#">Indonesia</a> | <a href="#">Ireland</a> |
+                    <a href="#">New Zealand</a> | <a href="#">United Kingdom</a> | <a href="#">Turkey</a> | <a
+                        href="#">Philippines</a> | <a href="#">Sri Lanka</a> | <a href="#">Australia</a> | <a
+                        href="#">Brasil</a> | <a href="#">Canada</a> | <a href="#">Chile</a> | <a href="#">Czech
+                        Republic</a> | <a href="#">India</a> | <a href="#">Indonesia</a> | <a href="#">Ireland</a> |
+                    <a href="#">New Zealand</a> | <a href="#">United Kingdom</a> | <a href="#">Turkey</a> | <a
+                        href="#">Philippines</a> | <a href="#">Sri Lanka</a>
+                </div>
+                <p class="mt-4 text-black">POPULAR FOOD</p>
+                <div class="search-links">
+                    <a href="#">Fast Food</a> | <a href="#">Chinese</a> | <a href="#">Street Food</a> | <a
+                        href="#">Continental</a> | <a href="#">Mithai</a> | <a href="#">Cafe</a> | <a href="#">South
+                        Indian</a> | <a href="#">Punjabi Food</a> | <a href="#">Fast Food</a> | <a
+                        href="#">Chinese</a> | <a href="#">Street Food</a> | <a href="#">Continental</a> | <a
+                        href="#">Mithai</a> | <a href="#">Cafe</a> | <a href="#">South Indian</a> | <a
+                        href="#">Punjabi Food</a><a href="#">Fast Food</a> | <a href="#">Chinese</a> | <a
+                        href="#">Street Food</a> | <a href="#">Continental</a> | <a href="#">Mithai</a> | <a
+                        href="#">Cafe</a> | <a href="#">South Indian</a> | <a href="#">Punjabi Food</a> | <a
+                        href="#">Fast Food</a> | <a href="#">Chinese</a> | <a href="#">Street Food</a> | <a
+                        href="#">Continental</a> | <a href="#">Mithai</a> | <a href="#">Cafe</a> | <a href="#">South
+                        Indian</a> | <a href="#">Punjabi Food</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="pt-4 pb-4 text-center">
+    <div class="container">
+        <p class="mt-0 mb-0">© Copyright 2020 Osahan Eat. All Rights Reserved</p>
+        <small class="mt-0 mb-0"> Made with <i class="fas fa-heart heart-icon text-danger"></i> by
+            <a class="text-danger" target="_blank" href="https://www.instagram.com/iamgurdeeposahan/">Gurdeep
+                Osahan</a> - <a class="text-primary" target="_blank" href="https://askbootstrap.com/">Ask
+                Bootstrap</a>
+        </small>
+    </div>
+</footer>
